2bdb03595c1bc8ab036011c5521e0c3639303edd
Erica Calogero <ecalogero@gmail.com>
2020-06-04 20:04:04 +0100

    updated migration file and README with migration instructions

--- model/database.js
+++ model/database.js
@@ -10,19 +10,84 @@ const con = mysql.createConnection({
   host: DB_HOST || "127.0.0.1",
   user: DB_USER || "root",
   password: DB_PASS,
-  database: DB_NAME || "facebook",
-  multipleStatements: true
+  database: DB_NAME || "farmers",
+  multipleStatements: true,
 });
 
-con.connect(function(err) {
+con.connect(function (err) {
   if (err) throw err;
   console.log("Connected!");
 
-  let sql =
-    "DROP TABLE if exists students; CREATE TABLE students(id INT NOT NULL AUTO_INCREMENT, firstname VARCHAR(40) not null, lastname VARCHAR(40) not null, PRIMARY KEY (id));";
-  con.query(sql, function(err, result) {
+  let sql = `DROP TABLE IF EXISTS products;
+    DROP TABLE IF EXISTS users;
+    DROP TABLE IF EXISTS markets;
+    DROP TABLE IF EXISTS users_markets;
+    
+    CREATE TABLE products (
+      id int NOT NULL AUTO_INCREMENT,
+      name varchar(255) NOT NULL,
+      unit_price decimal NOT NULL,
+      units varchar(4) NOT NULL,
+      description varchar(255) NOT NULL,
+      tax_code varchar(255) NOT NULL,
+      catery_id varchar(255) NOT NULL,
+      isPerishable binary(1) NOT NULL,
+      shelf_life_count int NOT NULL,
+      shelf_life_units varchar(255) NOT NULL,
+      seller_id int NOT NULL,
+      PRIMARY KEY (id)
+    
+    );
+    
+    CREATE TABLE users (
+      id int NOT NULL AUTO_INCREMENT,
+      firstname varchar(255) NOT NULL,
+      lastname varchar(255) NOT NULL,
+      email varchar(255) NOT NULL,
+      address1 varchar(255) NOT NULL,
+      postcode varchar(255) NOT NULL,
+      city varchar(255) NOT NULL,
+      location varchar(255) NOT NULL,
+      company_name varchar(255) NOT NULL,
+      company_no varchar(255) NOT NULL,
+      tel_no varchar(255) NOT NULL,
+      mob_no varchar(255) NOT NULL,
+      website varchar(255) NOT NULL,
+      isSeller binary(1) NOT NULL,
+      PRIMARY KEY (id)
+    
+    );
+    
+    CREATE TABLE markets (
+      id int NOT NULL AUTO_INCREMENT,
+      name varchar(255) NOT NULL,
+      email varchar(255) NOT NULL,
+      address1 varchar(255) NOT NULL,
+      postcode varchar(255) NOT NULL,
+      city varchar(255) NOT NULL,
+      location varchar(255) NOT NULL,
+      company_name varchar(255) NOT NULL,
+      company_no varchar(255) NOT NULL,
+      tel_no varchar(255) NOT NULL,
+      mob_no varchar(255) NOT NULL,
+      day varchar(10) NOT NULL,
+      start_time time NOT NULL,
+      end_time time NOT NULL,
+      webite varchar(255) NOT NULL,
+      PRIMARY KEY (id)
+    
+    );
+    
+    CREATE TABLE users_markets (
+      id int NOT NULL AUTO_INCREMENT,
+      market_id int NOT NULL,
+      user_id int NOT NULL,
+      PRIMARY KEY (id)
+    
+    );`;
+  con.query(sql, function (err, result) {
     if (err) throw err;
-    console.log("Table creation `students` was successful!");
+    console.log("Table creation `users, markets and products` was successful!");
 
     console.log("Closing...");
   });
