841009d0d44bf550684ecbcd469b16c90538023e
Cihem <cihemzine@gmail.com>
2020-06-07 01:17:00 +0200

    recipe fetched

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-import MapContainer from './MapContainer'
+
+
 
 function App() {
   return (
     <div className="App">
 
-      <MapContainer />
+     
+   
       
     </div>
   );
