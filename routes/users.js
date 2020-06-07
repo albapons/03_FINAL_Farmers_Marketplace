3be3de4b2f07d6f9e945456d9e6c0ef8de6aa859
Erica Calogero <ecalogero@gmail.com>
2020-06-07 20:16:11 +0100

    new routes added, GET and POST requests coded but not working yet

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  db(`SELECT * FROM users WHERE users.isSeller=1;`)
+    .then((results) => {
+      res.send(results.data);
+    })
+    .catch((err) => res.status(500).send(err));
 });
 
 module.exports = router;
