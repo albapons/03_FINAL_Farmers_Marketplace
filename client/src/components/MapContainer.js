abbcbc55beda07dfedaa5e8e7278ddc2120da885
Erica Calogero <ecalogero@gmail.com>
2020-06-16 10:40:03 +0100

    debugged the spatial search some more

--- client/src/components/MapContainer.js
+++ client/src/components/MapContainer.js
@@ -19,43 +19,20 @@ const center = {
 };
 
 let service = null;
-let map, infoWindow, myLocation;
-let bounds, location;
+let infoWindow;
+let myBounds;
 export class MapContainer extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      bounds: "",
       input: "",
       suggestions: [],
-      places: [
-        {
-          address1: "Hydethorpe Rd",
-          city: "London",
-          company_name: "London Farmers' Markets",
-          company_no: "3815770",
-          day: "Saturday",
-          email: "info@lfm.org.uk",
-          end_time: "13:00:00",
-          id: 1,
-          lat: 51.444015,
-          lng: -0.14432,
-          location: "bla",
-          mob_no: "0207833 0338",
-          name: "Balham Farmers' Market",
-          postcode: "SW12 OJA",
-          start_time: "09:00:00",
-          tel_no: "0207833 0338",
-          website: "http://www.lfm.org.uk",
-        },
-      ],
+      places: [],
       markets: [],
     };
   }
-
-  savePlace = (place) => {
-    this.setState({ places: [...this.state.places, place] });
-  };
 
   handleChange = (e) => {
     this.setState({ input: e.target.value });
@@ -67,29 +44,59 @@ export class MapContainer extends Component {
     }
   };
 
-  onMarkerClick = (props, marker, e) => {
-    console.log(props, marker, e);
+  onMarkerMouseover = (props, marker, e) => {
+    console.log(
+      "This is the props: ",
+      props,
+      "This is the marker: ",
+      marker,
+      "This is the event: ",
+      e
+    );
+    const infoWindow = new props.google.maps.InfoWindow({
+      content: `${props.name}`,
+    });
+    infoWindow.open(props.map, marker);
   };
 
   initPlaces(mapProps, map) {
     const { google } = mapProps;
-    service = new google.maps.places.PlacesService(map);
+    //service = new google.maps.places.PlacesService(map);
     //console.log(google.maps);
-    infoWindow = new google.maps.InfoWindow();
+
+    google.maps.event.addListener(map, "idle", function () {
+      let bounds = map.getBounds();
+      //this doesn't work
+      //this.setState({ bounds: bounds.toUrlValue() });
+      localStorage.setItem("bounds", bounds.toUrlValue());
+      //This doesn't work either..
+      //this.searchDB();
+      //neither does this
+      // for (let i = 0; i < this.state.places.length; i++) {
+      //   let location = {
+      //     lat: this.state.places[i].lat,
+      //     lng: this.state.places[i].lng,
+      //   };
+      //   myBounds.extend(location);
+      // }
+    });
   }
 
   // componentDidMount() {
-  //   let myBounds = document.getElementById("myMap").getBounds();
+  //   let myBounds = document.getElementById("myMap")?.getBounds();
   //   console.log("Here are the bounds: ", myBounds);
   // }
 
   // Here is the modified search using the geocode library to return lat,lng co-ords to draw on the map
   searchDB = async () => {
-    console.log(bounds.toJSON());
-    let res = await api.getMarketsFiltered(bounds.toJSON());
+    let res = await api.getMarketsFiltered(localStorage.getItem("bounds"));
     console.log(res.data);
     for (const record of res.data) {
       this.setState({ suggestions: [...this.state.suggestions, record] });
+      console.log("record id: ", record.id);
+      if (!this.state.places.some((el) => el.id == record.id)) {
+        this.setState({ places: [...this.state.places, record] });
+      }
     }
   };
 
@@ -104,12 +111,13 @@ export class MapContainer extends Component {
   render() {
     const { suggestions, places } = this.state;
 
-    bounds = new this.props.google.maps.LatLngBounds();
-    for (let i = 0; i < places.length; i++) {
-      let location = { lat: places[i].lat, lng: places[i].lng };
-      bounds.extend(location);
-      //console.log("Here are the LatLngBounds: ", bounds.toJSON());
-    }
+    myBounds = new this.props.google.maps.LatLngBounds();
+
+    console.log("There are this many places: ", places.length);
+    // for (let i = 0; i < places.length; i++) {
+    //   let location = { lat: places[i].lat, lng: places[i].lng };
+    //   myBounds.extend(location);
+    // }
 
     return (
       <div className="container" style={{ width: "100%" }}>
@@ -156,12 +164,16 @@ export class MapContainer extends Component {
                       </div>
                       <span className="text-muted">{place.address1}</span>
                     </div>
-
+                    <div>
+                      <span className="text-muted">{`Day: ${place.day}     ${place.start_time} - ${place.end_time}`}</span>
+                    </div>
                     <button
-                      className="btn btn-outline-primary"
-                      onClick={() => this.savePlace(place)}
+                      className="btn btn-link"
+                      onClick={() =>
+                        /* navigate to that market page*/ this.state
+                      }
                     >
-                      Show
+                      More Info
                     </button>
                   </li>
                 ))}
@@ -175,16 +187,23 @@ export class MapContainer extends Component {
               onReady={this.initPlaces}
               zoom={14}
               style={{ height: "500px", width: "500px" }}
-              bounds={bounds}
-              initialCenter={{ lat: places[0].lat, lng: places[0].lng }}
+              initialCenter={{ lat: localStorage.lat, lng: localStorage.lng }}
             >
-              {places.map((market) => (
+              {places.map((market, i) => (
                 <Marker
-                  onClick={this.onMarkerClick}
+                  onMouseover={this.onMarkerMouseover}
                   name={market.name}
                   position={{ lat: market.lat, lng: market.lng }}
-                  key={market.id}
+                  key={i}
                 />
+              ))}
+              {places.map((market, i) => (
+                <InfoWindow onClose={this.onInfoWindowClose}>
+                  <div>
+                    <h1>{market.name}</h1>
+                    <h2>{market.address1 + market.postcode + market.city}</h2>
+                  </div>
+                </InfoWindow>
               ))}
             </Map>
           </div>
