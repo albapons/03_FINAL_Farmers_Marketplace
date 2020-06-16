2f18c6be07f3d265d5c9578f54ac33bc140da033
Cihem <cihemzine@gmail.com>
2020-06-16 20:06:17 +0200

    saving

--- routes/users.js
+++ routes/users.js
@@ -5,16 +5,8 @@ var userShouldBeLoggedIn = require("../guards/userShoudBeLoggedIn");
 const db = require("../model/helper");
 require("dotenv").config();
 const supersecret = process.env.SUPER_SECRET;
-router.post("/login", function (req, res, next) {
-  // Login logic
-
-const db = require("../model/helper");
-require("dotenv").config();
-const supersecret = process.env.SUPER_SECRET;
-
 // POST LOGIN
 router.post("/login", function (req, res, next) {
-  
   const { username, password } = req.body;
   db(
     `SELECT * FROM users WHERE username = "${username}" AND password = "${password}";`
@@ -28,17 +20,10 @@ router.post("/login", function (req, res, next) {
     }
   });
 });
-
-router.get("/profile", userShouldBeLoggedIn, function (req, res, next) {
-  res.send({ msg: `Here is the private data for user ${userId}!` });
-});
-
-
 // GET PROFILE INFO
 router.get("/profile", userShouldBeLoggedIn, function (req, res, next) {
   res.send({ msg: `Here is the private data for user ${userId}!` });
 });
-
 /* GET users listing. */
 const getUsers = (req, res) => {
   db(`SELECT * FROM users;`)
@@ -47,22 +32,27 @@ const getUsers = (req, res) => {
     })
     .catch((err) => res.status(500).send(err));
 };
-
-/* GET users listing. */
 router.get("/", getUsers);
 // GET one user
 router.get("/:id", function (req, res, next) {
   const { id } = req.params;
-
-
   db(`SELECT * FROM users WHERE id = ${id}`)
     .then((results) => {
       res.send(results.data[0]);
     })
     .catch((err) => res.status(500).send(err));
 });
-
 // INSERT a new user into the DB
+router.post("/", function (req, res, next) {
+  const { firstname, lastname, email, password, username } = req.body;
+  db(`INSERT INTO users (firstname, lastname, email, password, username) 
+  VALUES ("${firstname}", "${lastname}", "${email}, "${password}", "${username}");`)
+    .then((results) => {
+      res.send({ msg: "Your data was inputted correctly!" });
+    })
+    .catch((err) => res.status(500).send(err));
+});
+// // INSERT a new user into the DB
 // router.post("/", function (req, res, next) {
 //   const {
 //     firstname,
@@ -81,44 +71,6 @@ router.get("/:id", function (req, res, next) {
 //     password,
 //     username,
 //   } = req.body;
-//   db(`INSERT INTO users (firstname, lastname, email, address1, postcode, city, location, company_name, company_no, tel_no, mob_no, website, isSeller, password, username) 
-//   VALUES ("${firstname}", "${lastname}", "${email}, "${address1}", "${postcode}", "${city}", "${location}", "${company_name}", "${company_no}", "${tel_no}","${mob_no}", "${website}", "${isSeller}", "${password}", "${username}");`)
-
-
-// INSERT a new user into the DB
-// router.post("/", function (req, res, next) {
-//   const { firstname, lastname, email, password, username } = req.body;
-
-//   db(`INSERT INTO users (firstname, lastname, email, password, username) 
-//   VALUES ("${firstname}", "${lastname}", "${email}, "${password}", "${username}");`)
-
-//     .then((results) => {
-//       res.send({ msg: "Your data was inputted correctly!" });
-//     })
-//     .catch((err) => res.status(500).send(err));
-// });
-
-
-// INSERT a new user into the DB
-// router.post("/", function (req, res, next) {
-//   const {
-//     firstname,
-//     lastname,
-//     email,
-//     address1,
-//     postcode,
-//     city,
-//     location,
-//     company_name,
-//     company_no,
-//     tel_no,
-//     mob_no,
-//     website,
-//     isSeller,
-//     password,
-//     username,
-//   } = req.body;
-
 //   db(`INSERT INTO users (firstname, lastname, email, address1, postcode, city, location, company_name, company_no, tel_no, mob_no, website, isSeller, password, username)
 //   VALUES ("${firstname}", "${lastname}", "${email}, "${address1}", "${postcode}", "${city}", "${location}", "${company_name}", "${company_no}", "${tel_no}","${mob_no}", "${website}", "${isSeller}", "${password}", "${username}");`)
 //     .then((results) => {
@@ -126,7 +78,6 @@ router.get("/:id", function (req, res, next) {
 //     })
 //     .catch((err) => res.status(500).send(err));
 // });
-
 // DELETE a user from the DB
 router.delete("/:id", function (req, res, next) {
   const { id } = req.params;
@@ -136,5 +87,4 @@ router.delete("/:id", function (req, res, next) {
     })
     .catch((err) => res.status(500).send(err));
 });
-
 module.exports = router;
