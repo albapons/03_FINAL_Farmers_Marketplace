fdddfa50c441757b195736ddee276bba229f17b0
albapons <albaponslloret@gmail.com>
2020-06-16 18:38:20 +0200

    changes in products.js

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -1,7 +1,32 @@
-import React from "react";
-import ProductList from "./ProductsList";
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import api from "../utils/apiProducts";
+import ProductsList from "./ProductsList";
 
 export default function Products(props) {
+  const [products, setProducts] = useState([]);
+  const [name, setSearch] = useState("");
+  const history = useHistory();
+
+  const performSearch = () => {
+    history.push(`/products?name=${name}`);
+  };
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getProductsFiltered = () => {
+    api.getProductsFiltered(name).then((response) => {
+      setProducts(response.data);
+    });
+  };
+
+  useEffect(() => {
+    performSearch();
+    getProductsFiltered();
+  }, [name]);
+
   return (
     <div className="container">
       <div className="row">
@@ -11,7 +36,26 @@ export default function Products(props) {
           <h5 className="subtitle">Get your products!</h5>
         </div>
       </div>
-      <ProductList />
+      <div className="row">
+        <div className="col-md-3 d-flex">
+          <div className="row">
+            <div className="md-form mb-0">
+              <input
+                type="text"
+                id="name"
+                name="name"
+                className="form-control"
+                value={name}
+                onChange={(e) => handleChange(e)}
+              />
+              <label htmlFor="name">Search...</label>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-md-9">
+        <ProductsList products={products} />
+      </div>
     </div>
   );
 }
--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,6 @@ var express = require("express");
 var router = express.Router();
 var jwt = require("jsonwebtoken");
 var userShouldBeLoggedIn = require("../guards/userShoudBeLoggedIn");
-const db = require("../model/helper");
-require("dotenv").config();
-const supersecret = process.env.SUPER_SECRET;
-router.post("/login", function (req, res, next) {
-  // Login logic
 
 const db = require("../model/helper");
 require("dotenv").config();
@@ -14,7 +9,6 @@ const supersecret = process.env.SUPER_SECRET;
 
 // POST LOGIN
 router.post("/login", function (req, res, next) {
-  
   const { username, password } = req.body;
   db(
     `SELECT * FROM users WHERE username = "${username}" AND password = "${password}";`
@@ -29,11 +23,6 @@ router.post("/login", function (req, res, next) {
   });
 });
 
-router.get("/profile", userShouldBeLoggedIn, function (req, res, next) {
-  res.send({ msg: `Here is the private data for user ${userId}!` });
-});
-
-
 // GET PROFILE INFO
 router.get("/profile", userShouldBeLoggedIn, function (req, res, next) {
   res.send({ msg: `Here is the private data for user ${userId}!` });
@@ -47,13 +36,11 @@ const getUsers = (req, res) => {
     })
     .catch((err) => res.status(500).send(err));
 };
-
-/* GET users listing. */
 router.get("/", getUsers);
+
 // GET one user
 router.get("/:id", function (req, res, next) {
   const { id } = req.params;
-
 
   db(`SELECT * FROM users WHERE id = ${id}`)
     .then((results) => {
@@ -64,40 +51,15 @@ router.get("/:id", function (req, res, next) {
 
 // INSERT a new user into the DB
 router.post("/", function (req, res, next) {
-  const {
-    firstname,
-    lastname,
-    email,
-    address1,
-    postcode,
-    city,
-    location,
-    company_name,
-    company_no,
-    tel_no,
-    mob_no,
-    website,
-    isSeller,
-    password,
-    username,
-  } = req.body;
-  db(`INSERT INTO users (firstname, lastname, email, address1, postcode, city, location, company_name, company_no, tel_no, mob_no, website, isSeller, password, username) 
-  VALUES ("${firstname}", "${lastname}", "${email}, "${address1}", "${postcode}", "${city}", "${location}", "${company_name}", "${company_no}", "${tel_no}","${mob_no}", "${website}", "${isSeller}", "${password}", "${username}");`)
-
-
-// INSERT a new user into the DB
-router.post("/", function (req, res, next) {
   const { firstname, lastname, email, password, username } = req.body;
 
   db(`INSERT INTO users (firstname, lastname, email, password, username) 
   VALUES ("${firstname}", "${lastname}", "${email}, "${password}", "${username}");`)
-
     .then((results) => {
       res.send({ msg: "Your data was inputted correctly!" });
     })
     .catch((err) => res.status(500).send(err));
 });
-
 
 // // INSERT a new user into the DB
 // router.post("/", function (req, res, next) {
@@ -130,6 +92,7 @@ router.post("/", function (req, res, next) {
 // DELETE a user from the DB
 router.delete("/:id", function (req, res, next) {
   const { id } = req.params;
+
   db(`DELETE FROM users WHERE id = ${id}`)
     .then((results) => {
       res.send({ msg: "Your data was deleted correctly!" });
